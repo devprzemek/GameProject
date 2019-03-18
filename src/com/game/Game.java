5160fed497053603278579744afdc3449a525818
Przemek <salamonikp@wp.pl>
2019-03-18 21:03:43 +0100

    Initial Commit: Basic Structures and simple shape classes.

---
+++ src/com/game/Game.java
@@ -0,0 +1,14 @@
+package com.game;
+
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable {
+
+    public void run(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
